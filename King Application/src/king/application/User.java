123bf56bca080d783ea07ce01583095238b76570
xloera <38555843+xloera@users.noreply.github.com>
2018-11-17 16:41:03 -0800

    Add files via upload

--- King Application/src/king/application/User.java
+++ King Application/src/king/application/User.java
@@ -13,9 +13,27 @@ public class User {
     private String password;
     private String username;
     private String name;
+    private static int xp;
+    private static int level;
+    private static int next_level;
     public void User(String name,String username,String password){
         this.password = password;
         this.name = name;
         this. username = username;
+        xp=0;
+        level=1;
+        next_level =100;
     }
+    public static void XPgain(int x){
+        xp += x;
+
+    }
+    public static void levelcheck(){
+        if (xp > next_level){
+            level +=1;
+            xp = xp-next_level;
+            next_level = 2 * next_level+next_level;
+        }
+    }
+    
 }
